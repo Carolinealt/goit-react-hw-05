9a90c80cb1f40ed858461e6b70db6bd8dfdbe62d
Your Name <e.scaroline02@gmail.com>
2024-07-15 11:40:00 +0300

    Update MovieDetailsPage.jsx

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -9,7 +9,7 @@ const MovieDetailsPage = () => {
 
   const [posterPath, setPosterPath] = useState(``);
   const location = useLocation();
-  const backLocation = useRef(location.state ?? "/movies");
+  const backLocation = useRef(location.state);
   const { movieId } = useParams();
   const defaultImg =
     "<https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg>";
@@ -75,7 +75,7 @@ const MovieDetailsPage = () => {
             </div>
           </li>
         </ul>
-        <Link to={backLocation.current} className={css.link}>
+        <Link to={backLocation.current || "/movies"} className={css.link}>
           back
         </Link>
 
